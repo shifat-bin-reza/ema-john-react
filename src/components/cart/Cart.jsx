c4dc044e941ee2b6daa6835086bb3b71ea8a22cc
shifat-bin-reza <m.shifatbinreza@gmail.com>
2023-03-29 16:00:51 +0600

    added grand total price

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,18 +5,22 @@ const Cart = ({ cart }) => {
   console.log(cart);
 
   let total = 0;
+  let shipping = 0;
   for (const product of cart) {
     total = total + product.price;
+    shipping = shipping + product.shipping;
   }
+  const tax = (total * 7) / 100;
+  const grandTotal = total + shipping + tax;
 
   return (
     <div className="cart-container">
       <h3 className="title-order-summery">Order summery</h3>
       <p>Selected Items: {cart.length}</p>
-      <p>Total Price: {total}</p>
-      <p>Total Shipping Charge: </p>
-      <p>Tax: </p>
-      <h4>Grand Total: </h4>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping Charge: ${shipping} </p>
+      <p>Tax: ${tax} </p>
+      <h4>Grand Total: ${grandTotal}</h4>
     </div>
   );
 };
